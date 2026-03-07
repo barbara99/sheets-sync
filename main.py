f589cddc8e7879b9941682820ff536a866d84be6
Barbara Asiamah <barbaraasiamah99@gmail.com>
2026-03-07 06:37:57 +0000

    Change START_ROW value from 30 to 23

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
-START_ROW    = 30
+START_ROW    = 23
 ID_COL       = 0
 MASTER_SS_ID = "1BkMncGrq2o26CF77x7ppuyM0xlOEJSA6xNeu7T5CIHQ"
 MASTER_SHEET = "Sheet7"
