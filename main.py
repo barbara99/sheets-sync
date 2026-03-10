c0c7fbcd82a35342f3107d926723f864077abf81
Barbara Asiamah <barbaraasiamah99@gmail.com>
2026-03-10 11:28:21 +0000

    Update START_ROW constant to 12

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
-START_ROW    = 8
+START_ROW    = 12
 ID_COL       = 0
 MASTER_SS_ID = "1BkMncGrq2o26CF77x7ppuyM0xlOEJSA6xNeu7T5CIHQ"
 MASTER_SHEET = "Sheet7"
